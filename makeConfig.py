f1d3e46651463a3ca0f1a1642223dfc7f88b8a3f
Dominic Smith <dom@domsmith.co.uk>
2021-03-13 11:54:29 +0000

    makeConfig is a shell file!

--- makeConfig.py
+++
@@ -1,5 +0,0 @@
-icehost=$(aws ssm get-parameters --names "/c105-icecast/host" --output text  --query "Parameters[*].{Value:Value}")
-iceport=$(aws ssm get-parameters --names "/c105-icecast/port" --output text  --query "Parameters[*].{Value:Value}")
-icemount=$(aws ssm get-parameters --names "/c105-icecast/mount" --with-decryption --output text  --query "Parameters[*].{Value:Value}")
-icepass=$(aws ssm get-parameters --names "/c105-icecast/password" --with-decryption --output text  --query "Parameters[*].{Value:Value}")
-printf 'icehost = \"%s\"\niceport = \"%s\"\nicemount=\"%s\"\nicepass=\"%s\"' $icehost $iceport $icemount $icepass | sed '1h;1!H;$!d;x;s/\r/''/g' | cat -v > config.py
