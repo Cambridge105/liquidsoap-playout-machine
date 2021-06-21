94055cc84690a46f04e60fb833039cec931c8fd0
Dominic Smith <dom@domsmith.co.uk>
2021-06-21 18:40:51 +0100

    Fix removal of Opam Depext in Opam 2.1

--- opamstart.sh
+++ opamstart.sh
@@ -1,5 +1,3 @@
 opam init --auto-setup --dot-profile=~/.bash_profile
-opam install -y depext
-opam depext taglib mad lame vorbis opus cry samplerate liquidsoap -y
-opam install -y taglib mad lame vorbis opus cry samplerate liquidsoap
+opam install taglib mad lame vorbis opus cry samplerate liquidsoap --confirm-level=unsafe-yes
 eval $(opam env)
